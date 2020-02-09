0429e392796a5ed419f9170e42e46bb5b1dc2c6b
Jelena-Milev <jeka.milev@gmail.com>
2020-02-09 14:18:47 +0100

    Jos jedna ispravka

--- register.php
+++ register.php
@@ -35,7 +35,7 @@
 		          <div class="form-group">
 		            <label for="email">Email adresa</label>
 		            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-		            <small id="e_error" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		            <small id="e_error" class="form-text text-muted">Vaši podaci neće biti deljeni.</small>
 		          </div>
 		          <div class="form-group">
 		            <label for="password">Lozinka</label>
